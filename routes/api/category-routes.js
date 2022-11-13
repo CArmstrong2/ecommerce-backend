1b4637e94f9708b0eebd3e30eea0745b8743cb5b
Beef Webster <c_goldsmith@yahoo.com>
2022-11-13 12:37:42 -0800

    added  routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,50 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const allCategories = Category.findAll();
+  const categories = allCategories.map((row) => row.get({plain:true}));
+  res.json(categories)
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const oneCategory = Category.findOne({
+    where:{
+      id:req.params.id,
+    }
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  const categoryBody = {
+    category_name: req.body.name
+  }
+  const newCategory = Category.create(categoryBody)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  const updateCategory = {
+    category_name: req.body.name
+  }
+  const updatedCategory = Category.update(updateCategory,{
+    where:{
+      id: req.params.id
+    }
+  })
+  res.json(updatedCategory)
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  const deleteCategory = Category.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  res.json(deleteCategory)
 });
 
 module.exports = router;
